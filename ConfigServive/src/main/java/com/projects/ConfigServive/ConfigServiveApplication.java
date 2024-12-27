7695b2898d3bf55f2cd84a3077932d73cf438768
Rishi Raj Chansauria <rishi.chansauria@gmail.com>
2024-12-27 16:38:17 +0530

    Added config server application to fetch config

---
+++ ConfigServive/src/main/java/com/projects/ConfigServive/ConfigServiveApplication.java
@@ -0,0 +1,13 @@
+package com.projects.ConfigServive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiveApplication.class, args);
+	}
+
+}
---
+++ ConfigServive/src/test/java/com/projects/ConfigServive/ConfigServiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ConfigServive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
